8f472f94b2c2244cba037b99e295d6cbed5f11d8
AlaaKhaledAhmed <alaa.dev90@gmail.com>
2023-01-17 00:06:39 +0300

    test

---
+++ lib/Admin/AddUser.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tikect/Data/Firebase.dart';
+
+import '../Colors/Colors.dart';
+import '../Funcations/Funcation.dart';
+import '../Icons/Icons.dart';
+import '../Messag/Messages.dart';
+
+class AddUser extends StatefulWidget {
+  const AddUser({Key? key}) : super(key: key);
+
+  @override
+  State<AddUser> createState() => _AddUserState();
+}
+
+class _AddUserState extends State<AddUser> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController idController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  String? type;
+  GlobalKey<FormState> addKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(context, 'Add New Event Owner'),
+      body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height - 120,
+          child: Card(
+            child: Form(
+                key: addKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: LayoutBuilder(
+                    builder: (context, constraints) =>
+                        NotificationListener<OverscrollIndicatorNotification>(
+                      onNotification:
+                          (OverscrollIndicatorNotification? overscroll) {
+                        overscroll!.disallowGlow();
+                        return true;
+                      },
+                      child: SingleChildScrollView(
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(
+                              minWidth: constraints.maxWidth,
+                              minHeight: constraints.maxHeight),
+                          child: IntrinsicHeight(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+//==============================name textField===============================================================
+                                textField(
+                                  context,
+                                  nameIcon,
+                                  "Name",
+                                  false,
+                                  nameController,
+                                  manditary,
+                                  //fillColor: textFieldColors3,
+                                ),
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+//============================== Email textField===============================================================
+                                textField(
+                                  context,
+                                  emailIcon,
+                                  "Email",
+                                  false,
+                                  emailController,
+                                  emile,
+                                ),
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+//============================== password textField===============================================================
+                           textField(
+                                  context,
+                                  passIcon,
+                                  "Password",
+                                  true,
+                                  passwordController,
+                                  password,
+                                ),
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+//============================== phone textField===============================================================
+                                textField(
+                                  context,
+                                  Icons.phone,
+                                  "Phone",
+                                  false,
+                                  phoneController,
+                                  (V){},
+                                ),
+                                SizedBox(height: 10.h),
+                                const Spacer(
+                                  flex: 1,
+                                ),
+//Add bottom==================================================================
+                              bottom(context, 'Add User', white, () {
+                                  FocusManager.instance.primaryFocus?.unfocus();
+                                  if (addKey.currentState?.validate() == true) {
+                                    lode(context, '', 'lode');
+                                    Firbase.singUpAccountEventOwner(
+                                            name: nameController.text,
+                                            email: emailController.text,
+                                            password: passwordController.text,
+                                             phone:phoneController.text)
+                                        .then((String v) {
+                                      print('================$v');
+                                      if (v == 'done') {
+                                        Navigator.pop(context);
+                                        Navigator.pop(context);
+                                        lode(context, addData, doneData);
+                                      } else if (v == 'weak-password') {
+                                        Navigator.pop(context);
+                                        lode(context, addData, weekPassword);
+                                      } else if (v == 'email-already-in-use') {
+                                        Navigator.pop(context);
+                                        lode(context, addData, emailFound);
+                                      } else {
+                                        Navigator.pop(context);
+                                        lode(context, addData, errorDat);
+                                      }
+                                    });
+                                  }
+                                },
+                                    backgroundColor: iconColor,
+                                    width: MediaQuery.of(context).size.width,
+                                    height: 60.h),
+
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )),
+            elevation: 10,
+          )),
+    );
+  }
+}
---
+++ lib/Admin/UpdateUser.dart
@@ -0,0 +1,153 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tikect/Data/Firebase.dart';
+
+import '../Colors/Colors.dart';
+import '../Funcations/Funcation.dart';
+import '../Icons/Icons.dart';
+import '../Messag/Messages.dart';
+
+
+class UpdateUser extends StatefulWidget {
+  String name;
+  String phone;
+
+  String docId;
+  UpdateUser(
+      {Key? key,
+      required this.name,
+      required this.phone,
+
+      required this.docId})
+      : super(key: key);
+  @override
+  State<UpdateUser> createState() => _UpdateUserState();
+}
+
+class _UpdateUserState extends State<UpdateUser> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController idController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  String? type;
+  GlobalKey<FormState> addKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    nameController.text = widget.name;
+    phoneController.text = widget.phone;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(context, 'Update User Information'),
+      body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height - 200,
+          child: Card(
+            child: Form(
+                key: addKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: LayoutBuilder(
+                    builder: (context, constraints) =>
+                        NotificationListener<OverscrollIndicatorNotification>(
+                      onNotification:
+                          (OverscrollIndicatorNotification? overscroll) {
+                        overscroll!.disallowGlow();
+                        return true;
+                      },
+                      child: SingleChildScrollView(
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(
+                              minWidth: constraints.maxWidth,
+                              minHeight: constraints.maxHeight),
+                          child: IntrinsicHeight(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+
+//==============================name textField===============================================================
+                               textField(
+                                  context,
+                                  nameIcon,
+                                  "Name",
+                                  false,
+                                  nameController,
+                                  manditary,
+                                  //fillColor: textFieldColors3,
+                                ),
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+//============================== phone textField===============================================================
+                                textField(
+                                  context,
+                                  passIcon,
+                                  "Phone",
+                                  false,
+                                  phoneController,
+                                  password,
+                                ),
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+                                const Spacer(
+                                  flex: 1,
+                                ),
+//update bottom==================================================================
+                               bottom(context, 'Update User', white, () {
+                                  FocusManager.instance.primaryFocus?.unfocus();
+                                  if (addKey.currentState?.validate() == true) {
+                                    lode(context, '', 'lode');
+                                    Firbase.updateEventOwner(
+                                            name: nameController.text,
+                                            docId: widget.docId,
+                                            phone: phoneController.text)
+                                        .then((String v) {
+                                      print('================$v');
+                                      if (v == 'done') {
+                                        Navigator.pop(context);
+                                        Navigator.pop(context);
+                                        lode(context, addData, doneData);
+                                      } else if (v == 'weak-password') {
+                                        Navigator.pop(context);
+                                        lode(context, addData, weekPassword);
+                                      } else if (v == 'email-already-in-use') {
+                                        Navigator.pop(context);
+                                        lode(context, addData, emailFound);
+                                      } else {
+                                        Navigator.pop(context);
+                                        lode(context, addData, errorDat);
+                                      }
+                                    });
+                                  }
+                                },
+                                    backgroundColor: iconColor,
+                                    width: MediaQuery.of(context).size.width,
+                                    height: 60.h),
+
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )),
+            elevation: 10,
+          )),
+    );
+  }
+}
